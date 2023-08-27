7989166116b608f5b65a714ee1650328004c3742
Ozzy <ozzysp@icloud.com>
2023-08-27 05:35:22 -0300

    Add user and account

--- sistemaBancarioPython.py
+++ sistemaBancarioPython.py
@@ -1,41 +1,64 @@
-class SistemaBancario:
-    def __init__(self):
+class Usuario:
+    def __init__(self, nome, data_nascimento, cpf):
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+
+
+class Conta:
+    def __init__(self, agencia, numero, titular):
+        self.agencia = agencia
+        self.numero = numero
+        self.titular = titular
         self.saldo = 0
         self.depositos = []
         self.saques = []
 
-    def depositar(self, valor):
-        if valor <= 0:
-            print("Valor inválido para depósito.")
-            return
-        self.saldo += valor
-        self.depositos.append(valor)
-        print(f"Depósito de R$ {valor:.2f} realizado com sucesso.")
 
-    def sacar(self, valor):
-        if valor <= 0 or valor > 500:
-            print("Valor inválido para saque.")
-            return
-        if len(self.saques) >= 3:
-            print("Limite diário de saques atingido.")
-            return
-        if self.saldo < valor:
-            print("Saldo insuficiente.")
-            return
-        self.saldo -= valor
-        self.saques.append(valor)
-        print(f"Saque de R$ {valor:.2f} realizado com sucesso.")
+class SistemaBancario:
+    def __init__(self):
+        self.usuarios = []
+        self.contas = []
 
-    def extrato(self):
-        print("Extrato bancário:")
-        if not self.depositos and not self.saques:
-            print("Não foram realizadas movimentações.")
-        else:
-            for idx, valor in enumerate(self.depositos, start=1):
-                print(f"{idx}. Depósito: R$ {valor:.2f}")
-            for idx, valor in enumerate(self.saques, start=len(self.depositos) + 1):
-                print(f"{idx}. Saque: R$ {valor:.2f}")
-        print(f"Saldo atual: R$ {self.saldo:.2f}")
+    def cadastrar_usuario(self, nome, data_nascimento, cpf):
+        for usuario in self.usuarios:
+            if usuario.cpf == cpf:
+                print("CPF já cadastrado. Não é possível cadastrar novamente.")
+                return
+        novo_usuario = Usuario(nome, data_nascimento, cpf)
+        self.usuarios.append(novo_usuario)
+        print("Usuário cadastrado com sucesso.")
+
+    def cadastrar_conta(self, cpf, agencia, numero):
+        usuario = None
+        for u in self.usuarios:
+            if u.cpf == cpf:
+                usuario = u
+                break
+
+        if usuario is None:
+            print("Usuário não encontrado.")
+            return
+
+        for conta in self.contas:
+            if conta.numero == numero:
+                print("Número da conta já cadastrado. Escolha outro número.")
+                return
+
+        nova_conta = Conta(agencia, numero, usuario)
+        self.contas.append(nova_conta)
+        print("Conta cadastrada com sucesso.")
+
+    def depositar(self, numero_conta, valor):
+        # Implementação do depósito
+
+    def sacar(self, numero_conta, valor):
+        # Implementação do saque
+
+    def extrato(self, numero_conta):
+        # Implementação do extrato
+
+    # Restante das funções do sistema
 
 
 def main():
@@ -43,26 +66,26 @@ def main():
 
     while True:
         print("\nEscolha uma operação:")
-        print("1. Depositar")
-        print("2. Sacar")
-        print("3. Extrato")
-        print("4. Sair")
+        print("1. Cadastrar Usuário")
+        print("2. Cadastrar Conta")
+        print("3. Depositar")
+        print("4. Sacar")
+        print("5. Extrato")
+        print("6. Sair")
 
         escolha = int(input("Digite o número da operação: "))
 
         if escolha == 1:
-            valor = float(input("Digite o valor do depósito: "))
-            banco.depositar(valor)
+            nome = input("Digite o nome do usuário: ")
+            data_nascimento = input("Digite a data de nascimento (dd/mm/aaaa): ")
+            cpf = input("Digite o CPF (xxx.xxx.xxx-xx): ")
+            banco.cadastrar_usuario(nome, data_nascimento, cpf)
         elif escolha == 2:
-            valor = float(input("Digite o valor do saque: "))
-            banco.sacar(valor)
-        elif escolha == 3:
-            banco.extrato()
-        elif escolha == 4:
-            print("Saindo do sistema.")
-            break
-        else:
-            print("Opção inválida. Por favor, escolha uma opção válida.")
+            cpf = input("Digite o CPF do titular da conta: ")
+            agencia = "0001-1"
+            numero = input("Digite o número da conta (xx.xxx-x): ")
+            banco.cadastrar_conta(cpf, agencia, numero)
+        # Restante das opções do menu
 
 
 if __name__ == "__main__":
